e9eb86de769487b9d6ff1331f46bc523fa72f752
Chase Colvin <colvin.chase@yahoo.com>
2021-09-02 12:17:28 -0500

    Update temp_conversion.py

--- Desktop/temp_conversion/temp_conversion.py
+++ Desktop/temp_conversion/temp_conversion.py
@@ -3,5 +3,5 @@ def convert(temp):
     farenheight = float(temp)
     celcius = (farenheight - 32) * .5556
     return celcius 
-#I used 100°F in this example but you could replace it with any value
+#I used 100°F (37.778°C) in this example, but you could replace it with any value
 print(convert(100))
